b6bc85b0b89428bc24b54489ce18702b9387f511
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2023-01-23 21:58:43 +0000

    finalised the foundation of the high score system

--- autoload/high_score_manager.gd
+++ autoload/high_score_manager.gd
@@ -1,14 +1,17 @@
 extends Node
 
+# limit for the number of scores on the high score list
 const MAX_HIGH_SCORE_COUNT = 100
 
+# holds the current score for the game in progress
 var current_score: int = 0
 
 var high_scores: Array = [
 	[200, "Jonathan"],
-	[190, "Jonathan"],
-	[180, "Jonathan"],
-	[170, "Jonathan"],
+	[200, "Steve"],
+	[200, "Andy"],
+	[200, "Pete"],
+	[180, "Jim"],
 	[160, "Steve"],
 	[150, "Steve"],
 	[140, "Pete"],
@@ -28,6 +31,10 @@ var high_scores: Array = [
 ]
 
 
+func _ready() -> void:
+	high_scores.sort_custom(self, "sort_by_score")
+
+
 func is_new_high_score(score: int = 0) -> bool:
 	for entry in high_scores:
 		if score > entry[0]:
@@ -38,7 +45,7 @@ func is_new_high_score(score: int = 0) -> bool:
 	
 	return false
 
-
+# add a high score to the list
 func add_high_score(score: int = 0, by: String = "Anon") -> void:
 	var new_high_score: Array = [score, by]
 	
@@ -56,7 +63,15 @@ func get_top_score() -> Array:
 	return high_scores[0]
 
 
+func get_number_of_high_scores() -> int:
+	return high_scores.size()
+
+
+# custom sort for the high score list. 
 static func sort_by_score(a, b) -> bool:
+	#Sorts the scores numerically in the event of a tie, scores are sorted alphabetically by name.
 	if a[0] > b[0]:
 		return true
-	return false
+	elif a[0] == b[0]:
+		return a[1] < b[1]
+	return false;
--- menu/game_over.gd
+++ menu/game_over.gd
@@ -1,18 +1,35 @@
-extends Control
+extends VBoxContainer
 
-var final_score: int = false
-
+var final_score: int = 0
+var displayed_score: int = 0
 var new_high_score: bool = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$LineEdit.grab_focus()
+	$FinalScore/Score.text = str(displayed_score)
 	final_score = HighScoreManager.current_score
 	new_high_score = HighScoreManager.is_new_high_score(final_score)
+	# play high score animation
+	if new_high_score:
+		new_high_score()
+		$MarginContainer/LineEdit.grab_focus()
+
+
+func _physics_process(delta: float) -> void:
+	# increase the displayed score old school counter style
+	if (displayed_score < final_score):
+		displayed_score += 10
+		$FinalScore/Score.text = str(displayed_score)
+
+
+func new_high_score() -> void:
+	$AnimationPlayer.play("HighScore")
+	$AnimationPlayer.queue("HighScoreFlash")
 
 
 func _on_LineEdit_text_entered(new_text: String) -> void:
-	$LineEdit.set_editable(false)
+	$MarginContainer/LineEdit.set_editable(false)
 	
 	if new_high_score:
 		HighScoreManager.add_high_score(final_score, new_text)
